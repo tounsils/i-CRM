0225f79684bd2104fd3af80b776121819b9da3b6
tounsils <tounsils@gmail.com>
2021-07-02 16:23:28 +0100

    Added model controller and blade (blank) view

--- resources/views/createuser.blade.php
+++ resources/views/createuser.blade.php
@@ -7,8 +7,12 @@
 <main id="main">
     <!-- ======= About Section ======= -->
     <section id="about" class="about">
+    <div class="container" data-aos="fade-up">
+        <div class="row gx-0">
     
-<div><h1>Welcome page </h1> </div>
+<div><h1>Create new user </h1> </div>
+</div></div>
+
     </section><!-- End About Section -->
 </main><!-- End #main -->
 
