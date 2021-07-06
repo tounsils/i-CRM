1c30df1e3ae4e32f6a615b229cebe6d7a877f7c6
tounsils <tounsils@gmail.com>
2021-07-06 18:29:34 +0100

    user Section

--- resources/views/createuser.blade.php
+++ resources/views/createuser.blade.php
@@ -8,8 +8,70 @@
     <!-- ======= About Section ======= -->
     <section id="about" class="about">
     <div class="container" data-aos="fade-up">
-        <div class="row gx-0">
-    
+
+        <header class="section-header">
+          <h2>Users</h2>
+          <p>Add new user</p>
+        </header>
+
+
+        <div class="col-xl-12 d-flex content">
+            <div class="row align-self-center gy-4">
+
+                <div class="col-md-6 icon-box" data-aos="fade-up">
+                  <i class="ri-line-chart-line"></i>
+                  <div>
+                    <h4>Corporis voluptates sit</h4>
+                    <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut aliquip</p>
+                  </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+          <div class="col-lg-12">
+            <form action="forms/contact.php" method="post" class="php-email-form">
+              <div class="row gy-4">
+
+                <div class="col-md-6">
+                  <input type="text" name="name" class="form-control" placeholder="Your Name" required>
+                </div>
+
+                <div class="col-md-6 ">
+                  <input type="email" class="form-control" name="email" placeholder="Your Email" required>
+                </div>
+
+                <div class="col-md-12">
+                  <input type="text" class="form-control" name="subject" placeholder="Subject" required>
+                </div>
+
+                <div class="col-md-12">
+                  <textarea class="form-control" name="message" rows="6" placeholder="Message" required></textarea>
+                </div>
+
+                <div class="col-md-12 text-center">
+                  <div class="loading">Loading</div>
+                  <div class="error-message"></div>
+                  <div class="sent-message">Your message has been sent. Thank you!</div>
+
+                  <button type="submit">Send Message</button>
+                </div>
+
+              </div>
+            </form>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </section><!-- End Contact Section -->
+
+
 <div><h1>Create new user </h1> </div>
 </div></div>
 
